9209c041da5acbc989d9423645cfe4ab1241648d
Nicolas Robledo <tomatekiwi84@gmail.com>
2025-08-07 13:54:45 -0300

    tercer commit

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+  },
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup()
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    config = function()
+      require("mason-tool-installer").setup({
+        ensure_installed = {
+          "lua_ls",
+          "stylua",
+        },
+      })
+    end,
+  },
+}
+
